fd7bb72b285144a29cd3de9583fadd07fd138763
fjvmc <francisco.jose.carneiro@gmail.com>
2021-10-25 22:19:33 +0100

    Versao 0

---
+++ src/com/francisco/Calculadora.java
@@ -0,0 +1,21 @@
+package com.francisco;
+
+public class Calculadora {
+
+    //construtores
+    public Calculadora (){
+    }
+    //metodos
+    public double suma( double num1, double num2){
+        return num1+num2;
+    }
+    public double menos(double num1, double num2){
+        return num1+num2;
+    }
+    public double multiplicacao(double num1, double num2){
+        return num1*num2;
+    }
+    public double divisao(double num1, double num2){
+        return num1/num2;
+    }
+}
