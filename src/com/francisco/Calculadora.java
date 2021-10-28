95bc8c4a9b2d284225df9dd5516827ff6878fd4c
fjvmc <francisco.jose.carneiro@gmail.com>
2021-10-28 18:15:40 +0100

    Versao 1

--- src/com/francisco/Calculadora.java
+++ src/com/francisco/Calculadora.java
@@ -9,7 +9,7 @@ public class Calculadora {
     public double suma( double num1, double num2){
         return num1+num2;
     }
-    public double menos(double num1, double num2){
+    public double subtracao(double num1, double num2){
         return num1+num2;
     }
     public double multiplicacao(double num1, double num2){
